fdd37acdf376982534c90c18f4e1b7e6d76a8a56
Feira3F <appfeira3f24@gmail.com>
2024-10-18 15:56:49 -0300

    adios passwords

--- backend/mail/mail.php
+++ backend/mail/mail.php
@@ -14,7 +14,7 @@ function configurarEmail()
         $mail->Host       = 'smtp.gmail.com';
         $mail->SMTPAuth   = true;
         $mail->Username   = 'appfeira3f24@gmail.com';
-        $mail->Password   = 'sbvp hmza vbzk hdkb';
+        $mail->Password   = '';
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
         $mail->Port       = 587;
 
